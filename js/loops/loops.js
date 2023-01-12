e3b5b6e5c4a12fa438f01911f30260e7f7dbdf29
dmawardi <dmawardi@gmail.com>
2023-01-12 22:06:25 +0800

    Added for in object loop to loops section

--- js/loops/loops.js
+++ js/loops/loops.js
@@ -30,3 +30,25 @@ for (let index = 0; index < listOfFriends.length; index++) {
   //   Append to HTML
   forDiv.innerHTML = forDiv.innerHTML + element + ", ";
 }
+
+// For/in
+// Used to iterate through objects
+const forInDiv = document.getElementById("forIn");
+
+const ranger = {
+  health: 150,
+  color: "red",
+  morphTime: 3,
+  special: "backflip-kick",
+};
+
+// for each key found in object
+for (const key in ranger) {
+  // Create p tag
+  const p = document.createElement("p");
+  // add details of key and value to p tag
+  p.innerHTML = `${key}: ${ranger[key]}`;
+
+  // add p to inner html of forIn div
+  forInDiv.append(p);
+}
